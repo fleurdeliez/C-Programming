fb141bb7f88bc08de781efaa9c9ca657c32279d4
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-05-18 12:58:31 +0800

    Changes to be committed: 	modified:   2. Looping Constructs/exercises/4.c

--- 2. Looping Constructs/exercises/4.c
+++ 2. Looping Constructs/exercises/4.c
@@ -11,3 +11,19 @@ Enter a number: 89
 Enter a number: 0
 Sum = 343.90
 */
+
+#include <stdio.h>
+
+int main() {
+    double number, sum = 0;
+
+    do {
+        printf("Enter a number: ");
+        scanf("%lf", &number);
+        sum += number;
+    } while (number != 0);
+
+    printf("Sum = %.2lf\n", sum);
+
+    return 0;
+}
