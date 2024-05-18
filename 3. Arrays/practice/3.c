0dd8d7914e57f149fbfd419788da1fe2794e38c0
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-05-18 17:21:27 +0800

    Changes to be committed: 	new file:   3. Arrays/practice/1.exe 	new file:   3. Arrays/practice/2.exe 	modified:   3. Arrays/practice/3.c 	new file:   3. Arrays/practice/3.exe

--- 3. Arrays/practice/3.c
+++ 3. Arrays/practice/3.c
@@ -1,29 +1,31 @@
+// 5.18.24
+
 #include <stdio.h>
 
 int main() {
-
     int n, count = 0;
-
     printf("Enter the number of elements to be stored in the array: ");
-    scanf("%d", &n); 
-
-    int arr[n]; 
+    scanf("%d", &n);
 
     printf("Input %d elements in the array:\n", n);
+    
+    int arr[n];
     for (int i = 0; i < n; i++) {
         printf("element - %d: ", i);
-        scanf("%d", &arr[i]); 
+        scanf("%d", &arr[i]);
+        
     }
 
-    // loop to count the duplicate elements
     for (int i = 0; i < n; i++) {
-        if (arr[i] == arr[i + 1]) {
-            count++;
-            break;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j] && arr[j] != -1) {
+                count++;
+                arr[j] = -1;
+            }
         }
     }
 
-    printf("Total number of duplicate elements found in the array is: %d ", count);
-    
+    printf("Total number of duplicate elements found in the array is: %d", count);
+
     return 0;
 }
