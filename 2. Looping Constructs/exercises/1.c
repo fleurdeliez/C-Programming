8e146903dfee817e6a016c4e865a20fbcd54c264
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-05-18 12:04:13 +0800

    Changes to be committed: 	new file:   2. Looping Constructs/exercises/1.c 	new file:   2. Looping Constructs/exercises/2.c 	new file:   2. Looping Constructs/exercises/3.c

---
+++ 2. Looping Constructs/exercises/1.c
@@ -0,0 +1,12 @@
+/*
+Write a program that will ask the user to input three integers. Call these three 
+integers as start, end and step, respectively. Assume that start is always less than end 
+and that step is a positive integer greater than zero. Thereafter, the program should 
+print the numbers such that the first item is start, the second item is equivalent to 
+the first item plus the value of step, the third item is equivalent to second item plus 
+the value of step and so on. The last value to be printed should not be greater than end. 
+For example, if start = 5, end = 10 and step = 2, then the output should be:
+5
+7
+9
+*/
---
+++ 2. Looping Constructs/exercises/2.c
@@ -0,0 +1,4 @@
+/*
+Write a C program that will print all the odd numbers from 1 to 100. 
+Write a C program that will generate the numbers from 100 down to 0, i.e., 100, 99, 98, 97, 96, 95, 94,93, 92, 91…0
+*/
