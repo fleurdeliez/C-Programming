3f3128b3ed63ca8b59d1ca486911c1eca5adeb0a
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-05-18 12:30:55 +0800

    Changes to be committed: 	modified:   2. Looping Constructs/practice/assessments/3.c 	modified:   2. Looping Constructs/practice/assessments/3.exe

--- 2. Looping Constructs/practice/assessments/3.c
+++ 2. Looping Constructs/practice/assessments/3.c
@@ -4,17 +4,13 @@
 
 int main() {
 
-    for (int i = 1; i <= 1; i++){
-        for (int j = 2; j <= 2; j++) {
-            for (int k = 3; k <= 3; k++) {
-                for (int l = 4; l <= 4; l++) {
-                    for (int m = 0; m <= 5; m++) {
-                        printf("%d", i);
-                    }
-                }
-            }
-        }
-    }
+    int rows = 5;
 
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
     return 0;
 }
