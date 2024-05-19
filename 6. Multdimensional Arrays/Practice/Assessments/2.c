8aee2385af71dde1b72ef34bd917216c642925e5
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-05-19 15:26:42 +0800

    Changes to be committed: 	modified:   6. Multdimensional Arrays/Assessment - Hands-On-Activity/2-product-inventory.c 	modified:   6. Multdimensional Arrays/Assessment - Hands-On-Activity/2-product-inventory.exe 	new file:   6. Multdimensional Arrays/Assessment - Hands-On-Activity/tempCodeRunnerFile.c 	new file:   6. Multdimensional Arrays/Assessment - Hands-On-Activity/tempCodeRunnerFile.exe 	modified:   6. Multdimensional Arrays/Practice/Assessments/2.c

---
+++ 6. Multdimensional Arrays/Practice/Assessments/2.c
@@ -0,0 +1,12 @@
+// 5.19.24
+
+#include <stdio.h>
+
+int main() {
+    int max_n;
+    printf("Enter the maximum number of products in the inventory: ");
+    scanf("%d", &max_n);
+    
+    
+    return 0;
+}
