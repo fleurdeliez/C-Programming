03549e043867988380c4096955e605a9af5b4d53
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-05-19 09:07:00 +0800

    Changes to be committed: 	modified:   6. Multdimensional Arrays/Practice/Exercises/2.c 	modified:   6. Multdimensional Arrays/Practice/Exercises/2.exe 	modified:   6. Multdimensional Arrays/Practice/Exercises/3.c 	new file:   6. Multdimensional Arrays/Practice/Exercises/3.exe

--- 6. Multdimensional Arrays/Practice/Exercises/2.c
+++ 6. Multdimensional Arrays/Practice/Exercises/2.c
@@ -8,10 +8,20 @@ int main() {
     scanf("%d", &n);
 
     float arr[n];
-    printf("Input %d elements in the array: \n", n);
+    printf("Input %d number of elements in the array: \n", n);
     for (int i = 0; i < n; i++) {
-        printf("Element %d: ");
+        printf("Element %d: ", i);
         scanf("%f", &arr[i]);        
+    }
+
+    printf("T\nhe floating-point values stored into the array are:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%.2f ", arr[i]);
+    }
+
+    printf("\nThe floating-point values stored into the array in reverse are:\n");
+    for (int i = n -1; i >= 0; i--) {
+        printf("%.2f ", arr[i]);
     }
 
     return 0;
---
+++ 6. Multdimensional Arrays/Practice/Exercises/3.c
@@ -0,0 +1,37 @@
+// 5.19.24
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Input the number of elements to be stored in the array: ");
+    scanf("%d", &n);
+    printf("Input %d elements in the array:\n", n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("Element %d: ", i);
+        scanf("%d", &arr[i]);        
+    }
+
+    int odd_arr[n], even_arr[n], index = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] %2 == 0) {
+            even_arr[index++] = arr[i];
+        } else {
+            odd_arr[index++] = arr[i];
+        }
+    }
+
+    printf("\nThe Even elements are:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", even_arr[i]);
+    }
+
+    printf("\nThe Odd elements are:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", odd_arr[i]);
+    }
+
+    return 0;
+}
