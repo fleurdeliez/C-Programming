5734553f295ca05db64380ff044767bbb0f7c684
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-05-20 10:37:41 +0800

    Changes to be committed: 	modified:   11. Sorting Algorithms/BubbleSort.c 	modified:   11. Sorting Algorithms/BubbleSort.exe

--- 11. Sorting Algorithms/BubbleSort.c
+++ 11. Sorting Algorithms/BubbleSort.c
@@ -1,50 +1,36 @@
-// Optimized implementation of Bubble sort
-#include <stdbool.h>
 #include <stdio.h>
 
-void swap(int* xp, int* yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-// An optimized version of Bubble Sort
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);
-                swapped = true;
+void bubbleSort(int arr[], int n){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (n-1); j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
-
-        // If no two elements were swapped by inner loop,
-        // then break
-        if (swapped == false)
-            break;
     }
 }
 
-// Function to print an array
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
+void printArr(int arr[], int n){
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
+    }
 }
 
-// Driver program to test above functions
-int main()
-{
-    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+int main() {
+    int n;
+    printf("Enter the number of elements to be stored in the array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("element %d: ", i+1);
+        scanf("%d", &arr[i]);        
+    }
+
     bubbleSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
+    printArr(arr, n);
     return 0;
 }
