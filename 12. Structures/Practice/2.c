11f2a90091d6242ae04686dbfea072cbbec2aecc
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-06-10 23:51:19 +0800

    Changes to be committed: 	modified:   12. Structures/Practice/2.c 	new file:   12. Structures/Practice/2.exe 	modified:   12. Structures/Practice/sp5.exe

---
+++ 12. Structures/Practice/2.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 10
+
+typedef struct {
+    int id;
+    char name[50];
+    int age;
+    char grade[5];
+    int mark[5];
+} database;
+
+void addStudent();
+void searchStudent();
+void displayStudents();
+
+int main() {
+    database student[MAX];
+    int choice, count = 0;
+
+    printf("\nStudent Database Management System:\n");
+
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Add a student\n");
+        printf("2. Search for a student\n");
+        printf("3. Display all students\n");
+        printf("4. Exit\n");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+        
+
+        switch (choice) {
+            case 1:
+                addStudent(student, &count);
+                break;
+            case 2:
+                searchStudent(student, count);
+                break;
+            case 3:
+                displayStudents(student, count);
+                break;
+            case 4:
+                printf("\nExiting the program...\n");
+                return 0;
+            default:
+                printf("\nInvalid Choice. Try again.\n");
+        }
+    };
+    return 0;
+}
+
+void addStudent(database student[], int *count){
+    if (*count < MAX) {
+        printf("\nEnter student ID: ");
+        scanf("%d", &student[*count].id);
+        getchar();
+        printf("Enter student name: ");
+        fgets(student[*count].name, sizeof(student[*count]), stdin);
+        printf("Enter student age: ");
+        scanf("%d", &student[*count].age);
+        printf("Enter student grade: ");
+        scanf("%s", &student[*count].grade);
+        printf("\nEnter marks for 5 subjects:\n");
+        for (int i = 0; i < 5; i++) {
+            printf("Subject %d: ", i+1);
+            scanf("%d", &student->mark[i]);
+        }
+        printf("\n\nStudent added successfully", );
+        *count++;
+    } else {
+        printf("\nStudent limit reached.\n");
+    }
+}
+void searchStudent(database student, int count){
+    printf("\nNo students added in the database.\n");
+}
+void displayStudents(database student, int count){
+
+}
