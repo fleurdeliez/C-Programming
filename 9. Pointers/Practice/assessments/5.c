ce3ef01a39b2eb6a6b7830ec2089ff0bf45ef726
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-06-10 13:12:16 +0800

    Changes to be committed: 	modified:   9. Pointers/Practice/assessments/5.c 	modified:   9. Pointers/Practice/assessments/5.exe

--- 9. Pointers/Practice/assessments/5.c
+++ 9. Pointers/Practice/assessments/5.c
@@ -1,31 +1,24 @@
-/*
-You have an array of integers data of size 10. Write a program that asks the user 
-to enter ten  integers and stores them in the array. Then, using pointers, 
-calculate and display the average  value of the elements in the array.
-*/
-
 #include <stdio.h>
 
 int main() {
-    int data[10];
+    int arr[10];
     int sum = 0;
-    int *ptr = data;
+    int *ptr = arr;
 
-    printf("Input 10 integers:\n");
+    printf("Input 10 integers in the array:\n");
     for (int i = 0; i < 10; i++) {
         printf("element - %d: ", i+1);
-        scanf("%d", ptr);
+        scanf("%d", &arr[i]);        
+    }
+
+    for (int i = 0; i < 10; i++) {
+        sum += *ptr;
         ptr++;
     }
 
-    ptr = data;
-    for (int i = 0; i < 10; i++) {
-        sum += *ptr;   
-        ptr++;     
-    }
+    float result = (float)sum / 10;
 
-    float ave = (float)sum / 10;
+    printf("\nAverage: %.2f", result);
 
-    printf("Average: %.2f", ave);
     return 0;
 }
