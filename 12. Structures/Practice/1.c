5bff76d917bd635a527ab5a942cce314d6ce292e
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-06-10 21:08:37 +0800

    Changes to be committed: 	modified:   12. Structures/Practice/1.c 	modified:   12. Structures/Practice/1.exe

--- 12. Structures/Practice/1.c
+++ 12. Structures/Practice/1.c
@@ -23,7 +23,7 @@ int main() {
         printf("1. Add an employee\n");
         printf("2. Display employee details\n");
         printf("3. Calculate average salary\n");
-        printf("4.Exit\n");
+        printf("4. Exit\n");
         printf("\nEnter your choice: ");
         scanf("%d", &choice);
         
@@ -34,10 +34,12 @@ int main() {
             case 2:
                 displayEmployees(employee, count);
                 break;
+            case 3:
+                calculateAveSalary(employee, count);
+                break;
             case 4:
                 printf("\nExiting the program...");
                 return 0;
-                break;
             default:
                 printf("\nInvalid choice. Try again.\n");
         }
@@ -63,13 +65,25 @@ void addEmployee(record employee[], int* count){
 
 void displayEmployees(record employee[], int count){
     if (count != 0) {
-        printf("\nEmployee Details:\n\n");
+        printf("\nEmployee Details:\n");
         for (int i = 0; i < count; i++) {
-            printf("ID: %d\n", employee[i].id);
+            printf("\nID: %d\n", employee[i].id);
             printf("Name: %s", employee[i].name);
             printf("Salary: %.2lf\n", employee[i].salary);
         }
     } else {
         printf("\nNo employees to display.\n");
+    }
+}
+
+void calculateAveSalary(record employee[], int count){
+    if (count != 0) {
+        double totalSalary = 0;
+        for (int i = 0; i < count; i++) {
+            totalSalary += employee[i].salary;
+        }
+        printf("\nAverage Salary: %.2lf\n", totalSalary / count);
+    } else {
+        printf("\nNo employees to calculate average salary.\n");
     }
 }
