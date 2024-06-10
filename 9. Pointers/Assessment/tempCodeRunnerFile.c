61abe6d39f9c1d74431aab80dc8426d5b8e4498a
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-06-10 09:03:34 +0800

    Changes to be committed: 	modified:   9. Pointers/Assessment/1-pointer-arithmetic.exe 	new file:   9. Pointers/Assessment/tempCodeRunnerFile.c 	modified:   9. Pointers/Practice/assessments/1.c 	modified:   9. Pointers/Practice/assessments/1.exe

---
+++ 9. Pointers/Assessment/tempCodeRunnerFile.c
@@ -0,0 +1,14 @@
+/*
+Imagine you have an array of integers, and you need to perform various operations 
+using  pointers: 
+1. Declare an integer array of size 5 and initialize it with some values. 
+2. Declare an integer pointer and assign it the address of the first element of the 
+array. 
+3. Use pointer arithmetic to perform the following operations: 
+a) Print the value at the third element of the array using the pointer. 
+b) Increment the pointer by 2 positions and print the value it points to. 
+c) Subtract 1 from the value pointed to by the pointer and print the result. 
+d) Move the pointer back to the first element and print the value using the pointer. 
+*/
+
+#include <stdio.h>
--- 9. Pointers/Practice/assessments/1.c
+++ 9. Pointers/Practice/assessments/1.c
@@ -2,18 +2,18 @@
 
 int main() {
     int arr[5] = {1,2,3,4,5};
-    int *ptr = arr;
+    int* ptr = arr;
 
-    printf("third element: %d\n", *(ptr+2));
+    printf("\nvalue at the third element: %d", *ptr+2);
 
-    ptr += 2;
-    printf("value at the pointer incremented by 2: %d\n", *ptr);
+    ptr+=2;
+    printf("\nvalue after increment: %d", *ptr);
 
     (*ptr)--;
-    printf("subract 1: %d\n", *ptr);
+    printf("\nvalue after subtraction: %d", *ptr);
 
-    ptr-=2;
-    printf("first element: %d\n", *ptr);
+    ptr -= 2;
+    printf("\nvalue after increment: %d", *ptr);
 
     return 0;
 }
