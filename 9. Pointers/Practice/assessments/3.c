4184c3f0b00b499d14c07b530071b177fa0dff08
Liezel Tamparong <152322260+fleurdeliez@users.noreply.github.com>
2024-06-10 09:29:28 +0800

    Changes to be committed: 	modified:   9. Pointers/Assessment/3-swapping-2-pointers.c 	modified:   9. Pointers/Practice/assessments/3.c 	modified:   9. Pointers/Practice/assessments/3.exe

--- 9. Pointers/Practice/assessments/3.c
+++ 9. Pointers/Practice/assessments/3.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
+
 void swapPointers(int*, int*);
 
 int main() {
-    int a = 1, b = 2;
-    int *ptrA = &a, *ptrB = &b;
+    int a = 5, *ptrA = &a;
+    int b = 6, *ptrB = &b;
 
-    printf("Before swapping:\n");
-    printf("Value of ptrA: %d\n", *ptrA);
-    printf("Value of ptrB: %d\n", *ptrB);
+    printf("\nBefore swapping: %d %d", *ptrA, *ptrB);
 
     swapPointers(ptrA, ptrB);
 
-    printf("\nAfter swapping:\n");
-    printf("Value of ptrA: %d\n", *ptrA);
-    printf("Value of ptrB: %d\n", *ptrB);
+    printf("\nAfter swapping: %d %d", *ptrA, *ptrB);
 
     return 0;
 }
 
-void swapPointers(int* x, int* y){
-    int temp = *x; 
-    *x = *y;
-    *y = temp;
+void swapPointers(int *n1, int *n2){
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
 }
